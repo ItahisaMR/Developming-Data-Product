4e883d51e0f84f21f63c402f247560a7c0fdce1c
ItahisaMR <itahisa@gmail.com>
2015-08-19 12:05:50 +0100

    Upload

---
+++ Aplicacion.R
@@ -0,0 +1,8 @@
+#install.packages
+
+library(shinyapps)
+library(BH)
+
+shinyapps::setAccountInfo(name='itahisamr', token='425A006AB7CCB13BC43B9BFCC875F0BB', secret='xIGwn2bpvb/sK4uIoEbUdrfWvTaaUiy7OlfOcA9j')
+
+shinyapps::deployApp('/Users/Itahisa/Documents/Develpming Data Product/Developming Data product/Developming-Data-Product')
